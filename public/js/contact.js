3922ad4b01db29b6dc278a8f44d9b0681358b0fc
Mia Bracken <miab@Mias-MBP.home>
2025-01-26 16:17:11 -0600

    Updated contact tab w/ text and fix layout of feature and about me.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,19 +1,36 @@
 (function() {
     "use strict"
+
+    let form = document.querySelectorAll('#contact-form')
     
     // document.querySelector("#contact-form-button").addEventListener("click", submitMail);
     document.querySelector("#contact-form-button").addEventListener("click", (event) => {
         event.preventDefault();
         event.stopPropagation();
+        let formValid = true;
+        if (!form.checkValidity()) {
+           formValid = false;
+        }
+        form.classList.add('was-validated');
+        if (formValid) {
+            sendTheEmail();
+        }
+
+    });
+    function sendTheEmail() {
         console.log("You clicked the submit button.");
-        let name = document.querySelector("#name").value;
+        let firstname = document.querySelector("#first-name").value;
+        let lastname = document.querySelector("#last-name").value;
         let email = document.querySelector("#mail").value;
         let message = document.querySelector("#msg").value;
-        console.log("Name: " + name);
+        console.log("First name: " + firstname);
+        console.log("Last name: " + lastname);
         console.log("Email: " + email);
-        console.log("Message: " + message);
-    })
+        console.log("Message: " + message);  
 
+    }   
+
+}());
 
 
 // function submitMail() {
@@ -27,5 +44,3 @@
 //     console.log("Email: " + email);
 //     console.log("Message: " + message);
 // }
-
-}());
