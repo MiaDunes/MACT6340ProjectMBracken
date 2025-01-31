7c354c4b964c950b6012944acb642f681026e316
Mia Bracken <miab@Mias-MBP.home>
2025-01-30 21:47:15 -0600

    Updated project click, style-background, in-html-js.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,11 @@
 (function() {
     "use strict"
 
-    document.querySelector("#project- button").addEventListener("click", project);
+//     document.querySelector("#project-button").addEventListener("click", project);
     
-    function project() {
-    console.log("You clicked the submit button.");
-}
+//     function project() {
+//     console.log("You clicked the Project button.");
+// }
 
     
 //     document.querySelector("#contact-form-button").addEventListener("click", submitMail);
